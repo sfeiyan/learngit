b31e69fc8bb51a49b8319fbec4e588d7a8b03340
sfiyan <sfiyan@163.com>
2020-10-12 11:07:16 +0800

    添加新文件userController

---
+++ UserInfoController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: hello-spring-boot
+ * @description: userInfo
+ * @author: ssy
+ * @create: 2020-09-11 16:51
+ **/
+@Controller
+@RequestMapping("/userInfo")
+public class UserInfoController {
+
+    /**
+     * 用户查询.
+     * @return
+     */
+    @RequestMapping("/userList")
+    @RequiresPermissions("userInfo:view")//权限管理;
+    public String userInfo(){
+        return "user/userInfo";
+    }
+
+    /**
+     * 用户添加;
+     * @return
+     */
+    @RequestMapping("/userAdd")
+    @RequiresPermissions("userInfo:add")//权限管理;
+    public String userInfoAdd(){
+        return "user/userInfoAdd";
+    }
+
+    /**
+     * 用户删除;
+     * @return
+     */
+    @RequestMapping("/userDel")
+    @RequiresPermissions("userInfo:del")//权限管理;
+    public String userDel(){
+        return "user/userInfoDel";
+    }
+}
